19c0d4ac93912450ca30eede014b8d7ba81168bb
SERGIO RODRIGUEZ CALVO <SERGIO.RODRIGUEZ.CALVO@ing.com>
2020-04-27 22:23:57 +0200

    index and CORS in nginx

--- hr-front/pages/index.vue
+++ hr-front/pages/index.vue
@@ -13,14 +13,26 @@
           <h1>Department</h1>
           <b-button variant="info" class="mb-2 float-right">New Department</b-button>
           <div>
-            <b-table hover :items="items_dept"></b-table>
+            <b-table hover :items="depts" :fields="['name']">
+              <template v-slot:cell(name)="row">
+                {{ row.value }}
+              </template>
+            </b-table>
           </div>
         </b-col>
         <b-col>
           <h1>Employee</h1>
           <b-button variant="info" class="mb-2 float-right">New Employee</b-button>
           <div>
-            <b-table hover :items="items_empl"></b-table>
+            <b-table hover :items="emplsParsed" :fields="['name','dept']">
+              <template v-slot:cell(name)="row">
+                <!--pre>{{row}}</pre-->
+                {{ row.value }}
+              </template>
+              <template v-slot:cell(dept)="row">
+                {{ row.value }}
+              </template>
+            </b-table>
           </div>
         </b-col>
       </b-row>
@@ -30,17 +42,20 @@
 
 <script>
 export default {
+    async asyncData({ $axios }) {
+      const depts = await $axios.$get('http://localhost:8080/api/v1/department')
+      const empls = await $axios.$get('http://localhost:8080/api/v1/employee')
+      const deptsParsed = depts.reduce(function(d) {
+        return { [d.id]: d.name };
+      });
+      const emplsParsed = empls.map(function(e) {
+         e.dept = deptsParsed[e.deptId];
+         return e;
+      });
+      return { depts, emplsParsed }
+    },
     data() {
-      return {
-        items_dept: [
-          { name: 'HR' },
-          { name: 'IT' }
-        ],
-        items_empl: [
-          { name: 'Lechowsky', dept: 'IT' },
-          { name: 'Serrodcal', dept: 'IT' }
-        ]
-      }
+      return {}
     }
   }
 </script>
