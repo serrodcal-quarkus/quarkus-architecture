f5c7442855f7b1c0e2020d1e1b1dc9f5fa354ca4
SERGIO RODRIGUEZ CALVO <SERGIO.RODRIGUEZ.CALVO@ing.com>
2020-05-01 12:35:19 +0200

    refacto front, simplify

--- hr-front/pages/index.vue
+++ hr-front/pages/index.vue
@@ -11,17 +11,17 @@
       <b-row>
         <b-col>
           <h1>Department</h1>
-          <b-button variant="info" class="mb-2 float-right">New Department</b-button>
+          <b-button variant="info" class="mb-2 float-right" @click="create('Create Department', $event.target)">New Department</b-button>
           <div>
             <b-table hover :items="depts" :fields="fields_depts">
               <template v-slot:cell(name)="row">
                 {{ row.value }}
               </template>
               <template v-slot:cell(actions)="row">
-                <b-button size="sm" variant="info" class="mr-1">
+                <b-button size="sm" variant="info" class="mr-1" @click="update(row.item, 'Update Department', $event.target)">
                   <b-icon icon="pencil-square"></b-icon>
                 </b-button>
-                <b-button size="sm" variant="danger" class="mr-1">
+                <b-button size="sm" variant="danger" class="mr-1" @click="deleteDepartment(row.item.id)">
                   <b-icon icon="trash2-fill"></b-icon>
                 </b-button>
               </template>
@@ -30,9 +30,9 @@
         </b-col>
         <b-col>
           <h1>Employee</h1>
-          <b-button variant="info" class="mb-2 float-right">New Employee</b-button>
+          <b-button variant="info" class="mb-2 float-right" @click="create('Create Employee', $event.target)">New Employee</b-button>
           <div>
-            <b-table hover :items="emplsParsed" :fields="fields_empls">
+            <b-table hover :items="empls" :fields="fields_empls">
               <template v-slot:cell(name)="row">
                 <!--pre>{{row}}</pre-->
                 {{ row.value }}
@@ -41,7 +41,7 @@
                 {{ row.value }}
               </template>
               <template v-slot:cell(actions)="row">
-                <b-button size="sm" variant="info" class="mr-1" @click="info(row.item, row.index, $event.target)">
+                <b-button size="sm" variant="info" class="mr-1" @click="update(row.item, 'Update Employee', $event.target)">
                   <b-icon icon="pencil-square"></b-icon>
                 </b-button>
                 <b-button size="sm" variant="danger" class="mr-1" @click="deleteEmployee(row.item.id)">
@@ -53,7 +53,20 @@
         </b-col>
       </b-row>
       <b-modal :id="formModal.id" :title="formModal.title" ok-only @hide="resetFormModal">
-        <p>Prueba</p>
+        <form ref="formModal"> <!-- @submit.stop.prevent="handleSubmit" -->
+          <b-form-group
+            :state="nameState"
+            label="Name"
+            label-for="name-input"
+            invalid-feedback="Name is required">
+            <b-form-input
+              id="name-input"
+              v-model="name"
+              :state="nameState"
+              required
+            ></b-form-input>
+          </b-form-group>
+        </form>
       </b-modal>
     </b-container>
   </div>
@@ -70,28 +83,18 @@ export default {
     async asyncData({ $axios }) {
       const depts = await $axios.$get('http://localhost:8080/api/v1/department')
       const empls = await $axios.$get('http://localhost:8080/api/v1/employee')
-      const deptsParsed = depts.reduce(function(d) {
-        return { [d.id]: d.name };
-      });
-      const emplsParsed = empls.map(function(e) {
-         if (e.deptId) {
-           e.dept = deptsParsed[e.deptId];
-           return e;
-         } else {
-           e.dept = "Unassigned";
-           return e;
-         }
-      });
-      return { depts, emplsParsed }
+      return { depts, empls }
     },
     data() {
       return {
         fields_empls: [
+          { key: 'id', label: 'ID'},
           { key: 'name', label: 'Full Name'},
-          { key: 'dept', label: 'Deptartment'},
+          { key: 'deptId', label: 'Deptartment'},
           { key: 'actions', label: 'Actions' }
         ],
         fields_depts: [
+          { key: 'id', label: 'ID'},
           { key: 'name', label: 'Full Name'},
           { key: 'actions', label: 'Actions' }
         ],
@@ -103,8 +106,27 @@ export default {
       }
     },
     methods: {
-      info(item, index, button) {
-        this.formModal.title = `Row index: ${index}`
+      getEmployees(empls, depts) {
+        const deptsParsed = depts.reduce(function(d) {
+          return { [d.id]: d.name };
+        });
+        const emplsParsed = empls.map(function(e) {
+           if (deptsParsed && e.deptId && deptsParsed[e.deptId]) {
+             e.dept = deptsParsed[e.deptId];
+             return e;
+           } else {
+             e.dept = "Unassigned";
+             return e;
+           }
+        });
+        return emplsParsed
+      },
+      create(index, button) {
+        this.formModal.title = `${index}`
+        this.$root.$emit('bv::show::modal', this.formModal.id, button)
+      },
+      update(item, index, button) {
+        this.formModal.title = `${index}`
         this.formModal.content = JSON.stringify(item, null, 2)
         this.$root.$emit('bv::show::modal', this.formModal.id, button)
       },
@@ -113,12 +135,40 @@ export default {
         this.formModal.content = ''
       },
       async deleteEmployee(id) {
-        const deleted = await this.$axios.$delete(`http://localhost:8080/api/v1/employee/${id}`)
-        //debugger
-        if (deleted.status == 200) {
-          const empls = await this.$axios.$get('http://localhost:8080/api/v1/employee')
-          this.empls = empls
+        let res = await this.$axios({
+            url: `http://localhost:8080/api/v1/employee/${id}`,
+            method: 'delete',
+            timeout: 8000
+        })
+        if(res.status == 200){
+          let res2 = await this.$axios({
+              url: 'http://localhost:8080/api/v1/employee',
+              method: 'get',
+              timeout: 8000
+          })
+          if (res2.status == 200) {
+            this.empls = res2.data
+          }
         }
+        return
+      },
+      async deleteDepartment(id) {
+        let res = await this.$axios({
+            url: `http://localhost:8080/api/v1/department/${id}`,
+            method: 'delete',
+            timeout: 8000
+        })
+        if(res.status == 200){
+          let depts = await this.$axios({
+              url: 'http://localhost:8080/api/v1/department',
+              method: 'get',
+              timeout: 8000
+          })
+          if (depts.status == 200) {
+            this.depts = depts.data
+          }
+        }
+        return
       }
     }
   }
