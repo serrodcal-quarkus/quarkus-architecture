d6b0437d2b5dbd6560a2192fb5b16c4ad4a82f2c
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-05-03 21:03:40 +0200

    removes type submit for update employee button and fix styles warnings

--- hr-front/pages/index.vue
+++ hr-front/pages/index.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">Human Resources</b-navbar-brand>
+      <b-navbar-brand href="#">
+        Human Resources
+      </b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
+      <b-navbar-toggle target="nav-collapse" />
     </b-navbar>
 
     <b-container class="bv-example-row">
       <b-row>
         <b-col>
           <h1>Department</h1>
-          <b-button variant="info" class="mb-2 float-right" @click="department(null, 'Create Department', $event.target)">New Department</b-button>
+          <b-button variant="info" class="mb-2 float-right" @click="department(null, 'Create Department', $event.target)">
+            New Department
+          </b-button>
           <div>
             <b-table hover :items="depts" :fields="fields_depts">
               <template v-slot:cell(name)="row">
@@ -19,10 +22,10 @@
               </template>
               <template v-slot:cell(actions)="row">
                 <b-button size="sm" variant="info" class="mr-1" @click="department(row.item, 'Update Department', $event.target)">
-                  <b-icon icon="pencil-square"></b-icon>
+                  <b-icon icon="pencil-square" />
                 </b-button>
                 <b-button size="sm" variant="danger" class="mr-1" @click="deleteDepartment(row.item.id)">
-                  <b-icon icon="trash2-fill"></b-icon>
+                  <b-icon icon="trash2-fill" />
                 </b-button>
               </template>
             </b-table>
@@ -30,7 +33,9 @@
         </b-col>
         <b-col>
           <h1>Employee</h1>
-          <b-button variant="info" class="mb-2 float-right" @click="employee(null, 'Create Employee', $event.target)">New Employee</b-button>
+          <b-button variant="info" class="mb-2 float-right" @click="employee(null, 'Create Employee', $event.target)">
+            New Employee
+          </b-button>
           <div>
             <b-table hover :items="empls" :fields="fields_empls">
               <template v-slot:cell(name)="row">
@@ -42,10 +47,10 @@
               </template>
               <template v-slot:cell(actions)="row">
                 <b-button size="sm" variant="info" class="mr-1" @click="employee(row.item, 'Update Employee', $event.target)">
-                  <b-icon icon="pencil-square"></b-icon>
+                  <b-icon icon="pencil-square" />
                 </b-button>
                 <b-button size="sm" variant="danger" class="mr-1" @click="deleteEmployee(row.item.id)">
-                  <b-icon icon="trash2-fill"></b-icon>
+                  <b-icon icon="trash2-fill" />
                 </b-button>
               </template>
             </b-table>
@@ -53,33 +58,46 @@
         </b-col>
       </b-row>
       <b-modal :id="employeeModal.id" :title="employeeModal.title" hide-footer @hide="resetEmployeeModal">
-        <form ref="employeeModal"> <!-- @submit.stop.prevent="handleSubmit" -->
+        <form ref="employeeModal">
+          <!-- @submit.stop.prevent="handleSubmit" -->
           <b-form-group
             label="Name"
             label-for="name-input"
-            invalid-feedback="Name is required">
+            invalid-feedback="Name is required"
+          >
             <b-form-input
               id="name-input"
               v-model="formEmployee.name"
               required
-            ></b-form-input>
+            />
           </b-form-group>
           <b-form-group
             label="Department"
             label-for="department-input"
-            invalid-feedback="Name is required">
+            invalid-feedback="Name is required"
+          >
             <b-form-select
               id="department-input"
               v-model="formEmployee.deptId"
               required
             >
-              <b-form-select-option :value="null">Please select an option</b-form-select-option>
-              <b-form-select-option v-for="dept in depts" :value="dept.id">{{ dept.name }}</b-form-select-option>
+              <b-form-select-option :value="null">
+                Please select an option
+              </b-form-select-option>
+              <b-form-select-option v-for="dept in depts" :key="dept.id" :value="dept.id">
+                {{ dept.name }}
+              </b-form-select-option>
             </b-form-select>
           </b-form-group>
-          <b-button type="submit" variant="primary" v-if="!isUpdatedMode" @click="createEmployee()">Create</b-button>
-          <b-button type="submit" variant="primary" v-if="isUpdatedMode" @click="updateEmployee()">Update</b-button>
-          <b-button type="reset" variant="danger">Reset</b-button>
+          <b-button v-if="!isUpdatedMode" type="submit" variant="primary" @click="createEmployee()">
+            Create
+          </b-button>
+          <b-button v-if="isUpdatedMode" variant="primary" @click="updateEmployee()">
+            Update
+          </b-button>
+          <b-button type="reset" variant="danger">
+            Reset
+          </b-button>
         </form>
       </b-modal>
     </b-container>
@@ -94,153 +112,153 @@ Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 
 export default {
-    async asyncData({ $axios }) {
-      const depts = await $axios.$get('http://localhost/api/v1/department')
-      const empls = await $axios.$get('http://localhost/api/v1/employee')
-      return { depts, empls }
+  async asyncData ({ $axios, env }) {
+    const depts = await $axios.$get(env.API_URL + '/department')
+    const empls = await $axios.$get(env.API_URL + '/employee')
+    return { depts, empls }
+  },
+  data () {
+    return {
+      isUpdatedMode: false,
+      fields_empls: [
+        { key: 'id', label: 'ID' },
+        { key: 'name', label: 'Full Name' },
+        { key: 'deptId', label: 'Deptartment' },
+        { key: 'actions', label: 'Actions' }
+      ],
+      fields_depts: [
+        { key: 'id', label: 'ID' },
+        { key: 'name', label: 'Full Name' },
+        { key: 'actions', label: 'Actions' }
+      ],
+      formEmployee: {
+        name: '',
+        deptId: null
+      },
+      formDepartment: {
+        name: ''
+      },
+      employeeModal: {
+        id: 'employee-modal',
+        title: '',
+        content: ''
+      },
+      departmentModal: {
+        id: 'department-modal',
+        title: '',
+        content: ''
+      }
+    }
+  },
+  methods: {
+    async loadPage () {
+      this.depts = await this.$axios.$get(process.env.API_URL + '/department')
+      this.empls = await this.$axios.$get(process.env.API_URL + '/employee')
     },
-    data() {
-      return {
-        isUpdatedMode: false,
-        fields_empls: [
-          { key: 'id', label: 'ID'},
-          { key: 'name', label: 'Full Name'},
-          { key: 'deptId', label: 'Deptartment'},
-          { key: 'actions', label: 'Actions' }
-        ],
-        fields_depts: [
-          { key: 'id', label: 'ID'},
-          { key: 'name', label: 'Full Name'},
-          { key: 'actions', label: 'Actions' }
-        ],
-        formEmployee: {
-          name: "",
-          deptId: null
-        },
-        formDepartment: {
-          name: ""
-        },
-        employeeModal: {
-          id: 'employee-modal',
-          title: '',
-          content: ''
-        },
-        departmentModal: {
-          id: 'department-modal',
-          title: '',
-          content: ''
+    createEmployee () {
+      this.$axios({
+        url: process.env.API_URL + '/employee',
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        data: {
+          name: this.formEmployee.name,
+          deptId: this.formEmployee.deptId
+        }
+      })
+        .then(function (response) {
+          if (response.status === 200) {
+            console.log('Create Success')
+            this.loadPage()
+          } else {
+            console.log('a tomar por culo ya')
+          }
+        })
+        .catch(function (response) {
+          console.log(response)
+          // resolve()
+        })
+    },
+    updateEmployee () {
+      // eslint-disable-next-line no-debugger
+      debugger
+      this.$axios({
+        url: process.env.API_URL + '/employee',
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        data: {
+          id: this.formEmployee.id,
+          name: this.formEmployee.name,
+          deptId: this.formEmployee.deptId
+        }
+      })
+        .then((res) => {
+          if (res.status === 200) {
+            this.loadPage()
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    employee (item, index, button) {
+      this.isUpdatedMode = false
+      this.employeeModal.title = `${index}`
+      if (item && item.id) {
+        this.formEmployee.name = item.name
+        this.formEmployee.deptId = item.deptId
+        this.isUpdatedMode = true
+      }
+      this.$root.$emit('bv::show::modal', this.employeeModal.id, button)
+    },
+    department (item, index, button) {
+      this.departmentModal.title = `${index}`
+      this.departmentModal.content = JSON.stringify(item, null, 2)
+      this.$root.$emit('bv::show::modal', this.departmentModal.id, button)
+    },
+    resetEmployeeModal () {
+      this.employeeModal.title = ''
+      this.employeeModal.content = ''
+    },
+    resetDepartmentModal () {
+      this.departmentModal.title = ''
+      this.departmentModal.content = ''
+    },
+    async deleteEmployee (id) {
+      const res = await this.$axios({
+        url: `${process.env.API_URL}/employee/${id}`,
+        method: 'delete',
+        timeout: 8000
+      })
+      if (res.status === 200) {
+        const res2 = await this.$axios({
+          url: process.env.API_URL + '/employee',
+          method: 'get',
+          timeout: 8000
+        })
+        if (res2.status === 200) {
+          this.empls = res2.data
         }
       }
     },
-    methods: {
-      loadPage() {
-        this.depts = this.$axios.$get('http://localhost/api/v1/department')
-        this.empls = this.$axios.$get('http://localhost/api/v1/employee')
-      },
-      createEmployee() {
-        const res = this.$axios({
-            url: 'http://localhost/api/v1/employee',
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            data: {
-              name: this.formEmployee.name,
-              deptId: this.formEmployee.deptId
-            }
+    async deleteDepartment (id) {
+      const res = await this.$axios({
+        url: `${process.env.API_URL}/department/${id}`,
+        method: 'delete',
+        timeout: 8000
+      })
+      if (res.status === 200) {
+        const depts = await this.$axios({
+          url: process.env.API_URL + '/department',
+          method: 'get',
+          timeout: 8000
         })
-        .then(function (response) {
-           if (response.status === 200) {
-             console.log("Create Success")
-             loadPage()
-           } else {
-             console.log("a tomar por culo ya")
-           }
-         })
-         .catch(function (response) {
-           console.log(response);
-           resolve();
-         });
-      },
-      updateEmployee() {
-        this.$axios({
-            url: 'http://localhost/api/v1/employee',
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            data: {
-              id: this.formEmployee.id,
-              name: this.formEmployee.name,
-              deptId: this.formEmployee.deptId
-            }
-        })
-         .then(res => {
-            if (res.status == 200) {
-              this.loadPage();
-            }
-         })
-         .catch(err => {
-             console.log(err);
-          });
-      },
-      employee(item, index, button) {
-        this.isUpdatedMode = false
-        this.employeeModal.title = `${index}`
-        if (item && item.id) {
-          this.formEmployee.name = item.name
-          this.formEmployee.deptId = item.deptId
-          this.isUpdatedMode = true
+        if (depts.status === 200) {
+          this.depts = depts.data
         }
-        this.$root.$emit('bv::show::modal', this.employeeModal.id, button)
-      },
-      department(item, index, button) {
-        this.departmentModal.title = `${index}`
-        this.departmentModal.content = JSON.stringify(item, null, 2)
-        this.$root.$emit('bv::show::modal', this.departmentModal.id, button)
-      },
-      resetEmployeeModal() {
-        this.employeeModal.title = ''
-        this.employeeModal.content = ''
-      },
-      resetDepartmentModal() {
-        this.departmentModal.title = ''
-        this.departmentModal.content = ''
-      },
-      async deleteEmployee(id) {
-        let res = await this.$axios({
-            url: `http://localhost/api/v1/employee/${id}`,
-            method: 'delete',
-            timeout: 8000
-        })
-        if(res.status == 200){
-          let res2 = await this.$axios({
-              url: 'http://localhost/api/v1/employee',
-              method: 'get',
-              timeout: 8000
-          })
-          if (res2.status == 200) {
-            this.empls = res2.data
-          }
-        }
-        return
-      },
-      async deleteDepartment(id) {
-        let res = await this.$axios({
-            url: `http://localhost/api/v1/department/${id}`,
-            method: 'delete',
-            timeout: 8000
-        })
-        if(res.status == 200){
-          let depts = await this.$axios({
-              url: 'http://localhost/api/v1/department',
-              method: 'get',
-              timeout: 8000
-          })
-          if (depts.status == 200) {
-            this.depts = depts.data
-          }
-        }
-        return
       }
     }
   }
+}
 </script>
 
 <style>
